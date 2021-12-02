8e4a8ed87c8442843507fa202d7c1c6d495632b8
Nate Bradshaw <nate.bradshaw5959@gmail.com>
2021-12-02 09:50:00 +0000

    done bubble sort

--- src/sort/bubbleSort/bubbleSort.ts
+++ src/sort/bubbleSort/bubbleSort.ts
@@ -1,11 +1,61 @@
+
 import { SortAlgorithm } from "../common";
 import swap from "../swap";
 
 const bubbleSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
 
-    // Your code here
+    let itemsOut = items; 
 
-    return items;
+    for(let i = 0; i<items.length-1; i++){
+        let swaped = false;
+
+        for(let j = 0; j<items.length-i-1; j++){
+            if(itemsOut[j] > itemsOut[j+1]){
+                swaped = true;
+                swap(itemsOut, j, j+1)
+            }
+        }
+
+        if(!swaped){
+            break;
+        }
+    }
+
+    return itemsOut;
 }
 
 export default bubbleSort;
+
+
+
+/*
+import { SortAlgorithm } from "../common";
+import swap from "../swap";
+
+const bubbleSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
+
+    //const arraySize: number = items.length;
+    for (let i = 0; i < (items.length - 1); i++) {
+
+        let swapMade: boolean = false;
+        for (let j = 0; j < (items.length - i - 1); j++) {
+            if (items[j] > items[j + 1]) {
+                swapMade = true;
+                swap(items, j, j + 1);
+
+            }
+
+        }
+
+        if (!swapMade) {
+            break;
+
+        }
+
+    }
+    return items;
+
+}
+
+export default bubbleSort;
+*/
