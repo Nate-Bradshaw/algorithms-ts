7bb6ac3eac2093c720d2d66b310eb613ba483fc7
Nate Bradshaw <nate.bradshaw5959@gmail.com>
2021-12-08 10:15:39 +0000

    INSERTION SORT COMPLETED

--- src/sort/insertionSort/insertionSort.ts
+++ src/sort/insertionSort/insertionSort.ts
@@ -3,9 +3,27 @@ import swap from "../swap";
 
 const insertionSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
 
-    // Your code here
+    let outList = items;
+    for(let i = 0; i < items.length; i++){
+        let itemToPlace = outList[i];
+        let itemPlace = i;
 
-    return items;
+        while(itemPlace>0){
+            let low = itemPlace - 1;
+            let high = itemPlace;
+
+            if(outList[low] > itemToPlace){
+                swap(outList, low, high);
+            }
+            else{
+                itemPlace = high;
+                break;
+            }
+            itemPlace -= 1;
+        }
+    }
+
+    return outList;
 }
 
-export default insertionSort;
+export default insertionSort; 
