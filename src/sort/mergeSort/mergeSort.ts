22bad0bd7a2d5cd7e664491bf10eadf2e6f53820
Nate Bradshaw <nate.bradshaw5959@gmail.com>
2021-12-09 13:05:11 +0000

    6 red lines sitting on a wall

--- src/sort/mergeSort/mergeSort.ts
+++ src/sort/mergeSort/mergeSort.ts
@@ -8,8 +8,8 @@ const mergeSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
 
     //Merge(items, 0, items.length - 1);
 
-    //return Merge(items, 0, items.length - 1); //???
-    return items;
+    return Merge(items, 0, items.length - 1); //???
+    //return items;
 }
 
 function Merge<T>(input_list: T[], left_pointer: number, right_pointer: number): T[]{
@@ -19,10 +19,10 @@ function Merge<T>(input_list: T[], left_pointer: number, right_pointer: number):
 
     let middle = Math.floor((left_pointer + right_pointer) / 2);
 
-    let first_half: [] = Merge(input_list, left_pointer, middle)
-    let second_half: [] = Merge(input_list, middle + 1, right_pointer);
+    let first_half: T[] = Merge(input_list, left_pointer, middle)
+    let second_half: T[] = Merge(input_list, middle + 1, right_pointer);
 
-    let output_list = [];
+    let output_list: T[] = [];
     while(first_half.length > 0 && second_half.length > 0){
         if(first_half[0] > second_half[0]){
             output_list.push(second_half.pop(0));
@@ -37,3 +37,5 @@ function Merge<T>(input_list: T[], left_pointer: number, right_pointer: number):
 
     return output_list;
 }
+
+export default mergeSort; 
