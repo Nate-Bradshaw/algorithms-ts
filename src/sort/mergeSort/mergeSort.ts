95a69ef1dd8a29bb75b49b384dbbfa946b909b7d
Nate Bradshaw <nate.bradshaw5959@gmail.com>
2021-12-10 10:47:29 +0000

    sharp fixes my problems

--- src/sort/mergeSort/mergeSort.ts
+++ src/sort/mergeSort/mergeSort.ts
@@ -19,21 +19,30 @@ function Merge<T>(input_list: T[], left_pointer: number, right_pointer: number):
 
     let middle = Math.floor((left_pointer + right_pointer) / 2);
 
-    let first_half: any = Merge(input_list, left_pointer, middle)
-    let second_half: any = Merge(input_list, middle + 1, right_pointer);
+    let first_half: T[] = Merge(input_list, left_pointer, middle)
+    let second_half: T[] = Merge(input_list, middle + 1, right_pointer);
 
-    let output_list: any = [];
+    let output_list: T[] = [];
     while(first_half.length > 0 && second_half.length > 0){
         if(first_half[0] > second_half[0]){
-            output_list.push(second_half.pop(0));
+            let item = second_half.shift();
+            if (!!item) output_list.unshift(item)
+
+            //output_list.push(second_half.pop());
         }
         else{
-            output_list.push(first_half.pop(0));
+            let item = second_half.pop();
+            if (!!item) output_list.push(item)
+            //output_list.push(first_half.pop());
         }
     }
 
-    output_list.push(first_half)
-    output_list.push(second_half)
+    // Push each item from the half
+    first_half.forEach((x: T) => output_list.push(x))
+    second_half.forEach((x: T) => output_list.push(x))
+0
+    //output_list.push(first_half)
+    //output_list.push(second_half)
 
     return output_list;
 }
