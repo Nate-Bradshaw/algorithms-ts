b2e495a287d9d48d7e3754d1fcc041e4d9c031fa
Nate Bradshaw <nate.bradshaw5959@gmail.com>
2021-12-10 10:32:08 +0000

    any = array spam in output

--- src/sort/mergeSort/mergeSort.ts
+++ src/sort/mergeSort/mergeSort.ts
@@ -19,10 +19,10 @@ function Merge<T>(input_list: T[], left_pointer: number, right_pointer: number):
 
     let middle = Math.floor((left_pointer + right_pointer) / 2);
 
-    let first_half: T[] = Merge(input_list, left_pointer, middle)
-    let second_half: T[] = Merge(input_list, middle + 1, right_pointer);
+    let first_half: any = Merge(input_list, left_pointer, middle)
+    let second_half: any = Merge(input_list, middle + 1, right_pointer);
 
-    let output_list: T[] = [];
+    let output_list: any = [];
     while(first_half.length > 0 && second_half.length > 0){
         if(first_half[0] > second_half[0]){
             output_list.push(second_half.pop(0));
